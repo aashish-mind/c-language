66599aff981d9997ca6057a7833f886aed148d86
aashish-mind <aashishbhawsar91@gmail.com>
2021-03-14 20:00:49 +0530

    created a foldre for strings programe

---
+++ strings-program/string.c
@@ -0,0 +1,28 @@
+/*
+main(){
+char s[13]="aashish bhawsar";
+int i;
+for(i=0;s[i]!='\0';i++)
+    printf("%c",s[i]);
+getch();}
+
+*/
+/*
+main(){
+char s [15];
+printf("enter a string");
+scanf("%s",&s[0]);
+puts(s);
+getch();
+}
+*/
+main(){
+char s [12];
+printf("enter a string");
+gets(s);
+puts(s);
+getch();}
+
+
+
+
